29965e513970f2b32f5651eba2591b07f653bae8
SEYL-Nima <93863425+SEYL-Nima@users.noreply.github.com>
2023-06-28 16:13:46 +0200

    first commit

---
+++ src/componants/testimonials/Testimonials.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './testimonials.css'
+
+const testimonials = () => {
+  return (
+    <div>testimonials</div>
+    <div> Still Working On it</div>
+  )
+}
+
+export default testimonials
